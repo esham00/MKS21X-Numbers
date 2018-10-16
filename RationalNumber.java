19c8699a010d283bcc0d28f4151fd21d7d54bdc9
Elizabeth Sham <esham00@stuy.edu>
2018-10-16 00:14:17 -0400

    Added constructors, methods, and returned dummy values

---
+++ RationalNumber.java
@@ -0,0 +1,25 @@
+public class RationalNumber extends RealNumber {
+  private double numer, denom;
+  public RationalNumber(RealNumber a, RealNumber b) {
+    //constructor
+  }
+  public RationalNumber() {
+    //constructor with a super
+  }
+  public double getNumber() {
+    //accessor method
+    return .1;
+  }
+  public String toString() {
+    //prints out the ratio not the number
+    return "";
+  }
+  public boolean isGreater(double nums) {
+    //compares the ratio of the rationalNumber to another getNumber
+    return .1;
+  }
+  public void setNum() {
+    //assigns a new number, making it immutable
+  }
+
+}
---
+++ RealNumber.java
@@ -0,0 +1,17 @@
+public class RealNumber {
+  private double num;
+  public RealNumber(double x) {
+    //constructor
+  }
+  public double getNumber() {
+    //accessor method
+    return 1.;
+  }
+  public String toString() {
+    //prints out the number
+    return "";
+  }
+  public void setNum(double i) {
+    //creates new number to assign to a value
+  }
+}
