16a5620977a3431300c881190ebba47b7297eb8f
Elizabeth Sham <esham00@stuy.edu>
2018-10-16 11:02:00 -0400

    Class

--- RealNumber.java
+++ RealNumber.java
@@ -1,17 +1,41 @@
-public class RealNumber {
-  private double num;
-  public RealNumber(double x) {
-    //constructor
+public class RealNumber{
+  private double value;
+
+  public RealNumber(double v){
+    value = v;
   }
-  public double getNumber() {
-    //accessor method
-    return 1.;
+
+  /*
+  *Return the sum of this and the other
+  */
+  public double add(RealNumber other){
+    return value + other.get();
   }
-  public String toString() {
-    //prints out the number
-    return "";
+  /*
+  *Return the product of this and the other
+  */
+  public double multiply(RealNumber other){
+    return value * other.get();
   }
-  public void setNum(double i) {
-    //creates new number to assign to a value
+  /*
+  *Return the this divided by the other
+  */
+  public double divide(RealNumber other){
+    return value / other.get();
+  }
+  /*
+  *Return the this minus the other
+  */
+  public double subtract(RealNumber other){
+    return value - other.get();
+  }
+
+
+  public double getValue(){
+    return value;
+  }
+
+  public String toString(){
+    return ""+value;
   }
 }
