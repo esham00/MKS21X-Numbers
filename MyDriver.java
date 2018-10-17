959e87b0000ed43fcebe1e6d79375ac572034032
Elizabeth Sham <esham00@stuy.edu>
2018-10-17 11:01:53 -0400

    added more

--- MyDriver.java
+++ MyDriver.java
@@ -13,6 +13,16 @@ public class MyDriver {
   System.out.println(zero.multiply(first) + "(0)");
   System.out.println(zero.divide(second) + "(0)");
   System.out.println(first.subtract(second) + "(-.5)");
-
+  System.out.println("Rational Numbers");
+  RationalNumber fourth = new RationalNumber(1,2);
+  RationalNumber fifth = new RationalNumber(1,-2);
+  RationalNumber sixth = new RationalNumber(-3,4);
+  RationalNumber seventh = new RationalNumber(5,10);
+  RationalNumber eighth = new RationalNumber(0,3);
+  RationalNumber ninth = new RationalNumber(5,1);
+  System.out.println(fourth + " (1/2)");
+  System.out.println(fifth + " (-1/2)");
+  System.out.println(sixth + " (-3/4)");
+  System.out.println(seventh + " (1/2)");
   }
 }
