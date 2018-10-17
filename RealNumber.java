b432188f74b7f4a47ecd3362087feb5f55600c60
Elizabeth Sham <esham00@stuy.edu>
2018-10-16 23:24:17 -0400

    works w Vincent Fong's driver

--- RealNumber.java
+++ RealNumber.java
@@ -9,25 +9,25 @@ public class RealNumber{
   *Return the sum of this and the other
   */
   public double add(RealNumber other){
-    return value + other.get();
+    return value + other.getValue();
   }
   /*
   *Return the product of this and the other
   */
   public double multiply(RealNumber other){
-    return value * other.get();
+    return value * other.getValue();
   }
   /*
   *Return the this divided by the other
   */
   public double divide(RealNumber other){
-    return value / other.get();
+    return value / other.getValue();
   }
   /*
   *Return the this minus the other
   */
   public double subtract(RealNumber other){
-    return value - other.get();
+    return value - other.getValue();
   }
 
 
